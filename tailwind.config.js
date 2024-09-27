4a29d7ba929d0df46189aa331880fea2756078c0
romainp-git <romain.portier@wanadoo.fr>
2024-09-27 13:34:53 +0200

    Mise en ligne de tous les fichiers

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.html',
+    './src/**/*.js',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
